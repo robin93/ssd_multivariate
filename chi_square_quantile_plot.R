4e37af20242c9373cabdeea5cab0b398719c6fb1
robin93 <robinsinghiitd@gmail.com>
2015-09-27 16:42:00 +0530

    Added the solution to 4_30 with the qq plots and the values of lambda for univariate

--- chi_square_quantile_plot.R
+++ chi_square_quantile_plot.R
@@ -59,4 +59,42 @@ par(mfrow = c(1,1))
 plot(chi_square_vals, sort(dist_alt))
 abline(a=0, b=1)
 
+#4.30 question data analysis
+#transformation lambda analysis
+x1_values <- c(1,2,3,3,4,5,6,8,9,11)
+x2_values <- c(18.95,19.00,17.95,15.54,14.00,12.95,8.94,7.49,6.00,3.99)
+l_value = numeric(0)
+log_array = log(x1_values)
+for (lambda in seq(from=0.01,to=1,by=0.01)){
+  first_term_sum=0
+  for (i in 1:length(x1_values)){
+    first_term_sum = first_term_sum+((x1_values[i])^lambda - mean(log_array))^2
+  }
+  l_value[lambda*100] = (lambda-1)*sum(log_array) + (-0.5)*(length(x1_values))*(log(first_term_sum/length(x1_values)))
+  }
+
+l_value
+plot(seq(from=0.01,to=1,by=0.01),l_value)
+
+l_value_2 = numeric(0)
+log_array = log(x2_values)
+for (lambda in seq(from=0.01,to=1,by=0.01)){
+  first_term_sum=0
+  for (i in 1:length(x2_values)){
+    first_term_sum = first_term_sum+((x2_values[i])^lambda - mean(log_array))^2
+  }
+  l_value_2[lambda*100] = (lambda-1)*sum(log_array) + (-0.5)*(length(x2_values))*(log(first_term_sum/length(x2_values)))
+}
+
+l_value_2
+plot(seq(from=0.01,to=1,by=0.01),l_value_2)
+
+#generating the Q-Q plots
+#for X1 values
+sorted_quantile_values_for_4_30 <- sort(c(rnorm(n=length(x1_values))))
+plot(sorted_quantile_values_for_4_30,sort(x1_values))
+plot(sorted_quantile_values_for_4_30,sort(x1_values^0.2))
+plot(sorted_quantile_values_for_4_30,sort(x2_values))
+plot(sorted_quantile_values_for_4_30,sort(x2_values^0.2))
+
 
